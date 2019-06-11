e1ffa0fdc8aab0f767a24aacb7aa6570da696a28
IndahSkr <indahlestari1004@gmail.com>
2019-06-11 14:17:41 +0700

    tambah.php

---
+++ page/tambah.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>.:: GRENNES (Grenn Unnes)::.</title>
+<link href="../page/admin.css" rel="stylesheet" type="text/css" />
+<script src="../page/js/calendar/datetimepicker.js" type="text/javascript"></script>
+<script src="tiny_mce/tiny_mce.js" type="text/javascript"></script>
+<script src="tiny_mce/tiny_gugun.js" type="text/javascript"></script>
+</head>
+<body>
+
+<div id="container_wrapper" method="post" action="cek_login.php">
+	<div id="container">
+	<div id="header">
+		<div id="inner_header">
+			<img style='width:100%' src='../page/images/k.jpg'>
+		</div>
+	</div>
+    <div id="top" align="center"> 
+		<span class="cpojer-links"> 
+			<a href="http://localhost/contoh/page/index.php">Home</a> 
+			<a>Soal</a>
+			<a href="jadwal.html">Hasil Survey</a> 
+			<a href="pemesanan.html">Indikator</a>
+			<a href="http://localhost/contoh/page/user.php">User</a>
+			<a>validasi</a>
+			<a href="lihat-detail-1.html">About us</a>
+			<a href="hubungi-kami.html">Hubungi Kami</a>
+		</span>
+	</div>
+	<table style='width:100%; padding-bottom:20px;'><tr></tr></table>
+  
+	<div id="left_column">
+		<a class='shiny-button'>Login User</a>
+		<div class="shiny-button">
+			<?php
+			session_start();
+			
+			//cek apakah yang mengakses halaman ini sudah login
+			if($_SESSION['jabatan']==""){
+				header("location:awal.php?pesan=gagal");
+			}
+			
+			?>
+			<p>Halo <b><?php echo $_SESSION['username']; ?></b> Anda telah login sebagai <b><?php echo $_SESSION['jabatan']; ?></b>.</p>
+			<a href="logout.php">LOGOUT</a>
+		</div>
+		<a class='shiny-button'>Alamat</a>
+		<div class='bawah'>
+		Unniversitas Negeri Semarang
+		Kampus Pusat: Kampus Sekaran Gunungpati
+		Semarang
+		</div>
+	</div>
+
+	<div id="right_column">
+		<br/>
+		<a href="http://localhost/contoh/page/user.php">Kembali</a>
+		<br/>
+		<br/>
+		<h3> Tambah Data User</h3>
+		<form method="post" action="tambah_aksi.php">
+			<table>
+				<tr>
+					<td>Username</td>
+					<td><input type="text" name="username"></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td><input type="password" name="password"></td>
+				</tr>
+				<tr>
+					<td>jabatan</td>
+					<td><select name="jabatan">
+				  		<option selected>Pilih Jabatan</option>
+						<option value="admin">Admin</option>
+			  			<option value="member">Member</option>
+					</select></td>
+				</tr>
+				<tr>
+					<td>Tanggal Lahir</td>
+					<td><input type="date" name="tanggal_lahir" value="2018-07-22" min="1999-01-01" max="2018-12-31"></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" class="tombol" value="SIMPAN"></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+
+	<div style='color:#fff;' id="footer">
+		Copyright (c) 2019 - GRENNES (Green Unnes)
+    </div>
+        
+</div>
+<div class="spacer"></div>
+</div>
+</body>
+</html>
