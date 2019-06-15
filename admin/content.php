c6ddba1231e9ba1f937a84205adf7bb7506e9c27
Qomariah Najmi <50134250+QomariahAmi@users.noreply.github.com>
2019-06-15 12:10:43 +0700

    Update content.php

--- admin/content.php
+++ admin/content.php
@@ -1,9 +1,9 @@
 <?php
 include "../config/koneksi.php";
 include "../config/library.php";
-include "../config/fungsi_indotgl.php";
+//include "../config/fungsi_indotgl.php";
 include "../config/class_paging.php";
-include "../config/fungsi_rupiah.php";
+//include "../config/fungsi_rupiah.php";
 include "../config/session_admin.php";
 
 if ($_GET[module]=='home'){
@@ -16,19 +16,19 @@ elseif ($_GET[module]=='user'){
 elseif ($_GET[module]=='daftaruser'){
   include "modul/mod_users/users.php";
 }
-elseif ($_GET[module]=='rekening'){
+//elseif ($_GET[module]=='rekening'){
   include "modul/mod_rekening/rekening.php";
 }
 
-elseif ($_GET[module]=='jam'){
+//elseif ($_GET[module]=='jam'){
   include "modul/mod_jam/jam.php";
 }
 
-elseif ($_GET[module]=='hubungi'){
+//elseif ($_GET[module]=='hubungi'){
   include "modul/mod_hubungi/hubungi.php";
 }
 
-elseif ($_GET[module]=='konfirmasi'){
+//elseif ($_GET[module]=='konfirmasi'){
   include "modul/mod_konfirmasi/konfirmasi.php";
 }
 
@@ -36,11 +36,11 @@ elseif ($_GET[module]=='about'){
   include "modul/mod_about/about.php";
 }
 
-elseif ($_GET[module]=='order'){
+//elseif ($_GET[module]=='order'){
   include "modul/mod_order/order.php";
 }
 
-elseif ($_GET[module]=='pesanan'){
+//elseif ($_GET[module]=='pesanan'){
   include "modul/mod_statis/statis.php";
 }
 
